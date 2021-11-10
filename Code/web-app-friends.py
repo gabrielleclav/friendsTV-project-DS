f07f5eb17b77e1f4313133995f3e10df5607ffb3
Gabby Clavell🧸 <gabbyclavell@Gabbys-MacBook-Pro.local>
2021-11-09 23:42:15 -0500

    most done

---
+++ Code/web-app-friends.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import streamlit as st
+import pickle 
+
+st.set_page_config(
+    page_title='Who are you more like from the TV Show Friends :)',
+    page_icon= '🛋', #command control space
+    initial_sidebar_state = 'expanded'
+)
