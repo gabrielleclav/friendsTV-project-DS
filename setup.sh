af3d3e8e23aead54df3c140a329ac39648c8ae01
Gabby Clavell🧸 <gabbyclavell@Gabbys-MacBook-Pro.local>
2021-11-15 12:20:22 -0500

    fixing custom theme

--- setup.sh
+++ setup.sh
@@ -1,8 +1,21 @@
 mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# \n\
+# " > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
