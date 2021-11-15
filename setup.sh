11159ad55f1c2cbc9a7414b7928b2cfb3816d4de
Gabby Clavell🧸 <gabbyclavell@Gabbys-MacBook-Pro.local>
2021-11-15 13:25:19 -0500

    trying custom

--- setup.sh
+++ setup.sh
@@ -1,19 +1,39 @@
 mkdir -p ~/.streamlit/
 echo "\
+[theme]\n\
+primaryColor = '#5093F6'\n\
+backgroundColor = '#000000'\n\
+secondaryBackgroundColor = '#3E5661'\n\
+textColor= '#FFFFFF'\n\
+font = 'sans serif'\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
-mkdir -p ~/.streamlit/
+# " > ~/.streamlit/config.toml
+
+
+# mkdir -p ~/.streamlit/
+# echo "\
+# [server]\n\
+# headless = true\n\
+# port = $PORT\n\
+# enableCORS = false\n\
+# \n\
+# " > ~/.streamlit/config.toml
+# # mkdir -p ~/.streamlit/
 
 # echo "[theme]
-# primaryColor = ‘#84a3a7’
-# backgroundColor = ‘#EFEDE8’
-# secondaryBackgroundColor = ‘#fafafa’
-# textColor= ‘#424242’
-# font = ‘sans serif’
+
+# primaryColor = '#5093F6' #blue
+
+# backgroundColor = '#000000' #black
+
+# secondaryBackgroundColor = '#3E5661'
+
+# textColor = '#FFFFFF' #white
+
+# font = "sans serif"
 # [server]
 # headless = true
 # port = $PORT
