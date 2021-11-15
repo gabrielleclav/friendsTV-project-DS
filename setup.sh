897b7de41466208fdd683815465bd609206043d9
Gabby Clavell🧸 <gabbyclavell@Gabbys-MacBook-Pro.local>
2021-11-15 12:06:23 -0500

    trying to deploy custom theme

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor = '#5093F6'
-backgroundColor = '#000000'
-secondaryBackgroundColor = '#3E5661'
-textColor= '#FFFFFF'
+primaryColor=’#5093F6
+backgroundColor=’#000000
+secondaryBackgroundColor=’#3E5661
+textColor = '#FFFFFF'
 font = ‘sans serif’
 [server]
 headless = true
